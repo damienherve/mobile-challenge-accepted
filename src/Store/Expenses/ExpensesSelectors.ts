f0c4690e0c6444ac2e06641f48eddd5425766c6b
Damien Hervé <herve.dami@gmail.com>
2020-03-06 18:42:34 +0100

    WIP - Search by merchant or user

--- src/Store/Expenses/ExpensesSelectors.ts
+++ src/Store/Expenses/ExpensesSelectors.ts
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect'
 import { ExpensesState } from '../types'
+import _ from 'lodash'
 
 export const getExpenses = (state: ExpensesState) => Object.values(state.data)
 
@@ -8,12 +9,31 @@ export const getExpensesUI = (state: ExpensesState) => state.ui
 export const canFetchMoreExpenses = (state: ExpensesState) =>
   getExpenses(state).length < state.ui.total
 
-export const getFilteredExpenses = createSelector(
+export const getFilteredExpensesByMerchant = createSelector(
   getExpenses,
   getExpensesUI,
   (expenses, expensesUI) => {
     return expenses.filter(expense =>
       expense.merchant.match(new RegExp(expensesUI.searchFilter, 'i'))
     )
+  }
+)
+
+export const getFilteredExpensesByUser = createSelector(
+  getExpenses,
+  getExpensesUI,
+  (expenses, expensesUI) => {
+    return expenses.filter(expense => {
+      const user = expense.user.first + ' ' + expense.user.last
+      return user.match(new RegExp(expensesUI.searchFilter, 'i'))
+    })
+  }
+)
+
+export const getFilteredExpenses = createSelector(
+  getFilteredExpensesByMerchant,
+  getFilteredExpensesByUser,
+  (expensesByMerchant, expensesByUser) => {
+    return _.union(expensesByMerchant, expensesByUser)
   }
 )
