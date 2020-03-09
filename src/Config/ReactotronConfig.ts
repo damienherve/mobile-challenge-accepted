6d848e30dc184e5a6c8def6848ed87b7970c52bd
Damien Hervé <herve.dami@gmail.com>
2020-03-09 16:35:07 +0100

    FIX - Minor issues on Android

--- src/Config/ReactotronConfig.ts
+++ src/Config/ReactotronConfig.ts
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-native'
 import { reactotronRedux as reduxPlugin } from 'reactotron-redux'
 import sagaPlugin from 'reactotron-redux-saga'
+import { AsyncStorage } from 'react-native'
 
 let reactotron = Reactotron.configure({
   name: 'Pleo Mobile Challenge'
 })
+  .setAsyncStorageHandler(AsyncStorage)
   .useReactNative()
   .use(reduxPlugin())
   .use(sagaPlugin({}))
